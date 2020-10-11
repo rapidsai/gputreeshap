267acc91fcf6a68639fbb51ad663aee6ccbd57a7
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2020-10-11 13:32:20 +1300

    Update ci

--- ci/checks/style.sh
+++ ci/checks/style.sh
@@ -7,28 +7,13 @@
 # Ignore errors and set path
 set +e
 PATH=/conda/bin:$PATH
+RETVAL="0"
 
 # Activate common conda env
 source activate gdf
 
-# Check for copyright headers in the files modified currently
-COPYRIGHT=`python ci/checks/copyright.py 2>&1`
-CR_RETVAL=$?
-if [ "$RETVAL" = "0" ]; then
-  RETVAL=$CR_RETVAL
-fi
-
-# Output results if failure otherwise show pass
-if [ "$CR_RETVAL" != "0" ]; then
-  echo -e "\n\n>>>> FAILED: copyright check; begin output\n\n"
-  echo -e "$COPYRIGHT"
-  echo -e "\n\n>>>> FAILED: copyright check; end output\n\n"
-else
-  echo -e "\n\n>>>> PASSED: copyright check\n\n"
-fi
-
-
 # Check for a consistent code format
+pip install cpplint
 FORMAT=`cpplint --recursive GPUTreeShap tests example 2>&1`
 FORMAT_RETVAL=$?
 if [ "$RETVAL" = "0" ]; then
