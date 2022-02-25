cdcff3fca252fd2cdbd63635ece0da87848a0458
Rory Mitchell <r.a.mitchell.nz@gmail.com>
2022-02-25 11:02:26 +0100

    Add doxygen (#33)
    * Warnings as errors
    
    * Doxygen
    
    * Use conda in CI

--- benchmark/benchmark_gpu_treeshap.cu
+++ benchmark/benchmark_gpu_treeshap.cu
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, NVIDIA CORPORATION.
+ * Copyright (c) 2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,11 +17,12 @@
 #include <benchmark/benchmark.h>
 #include "../tests/test_utils.h"
 
-using namespace gpu_treeshap;  // NOLINT
+
+using namespace gpu_treeshap; // NOLINT
 
 class Fixture : public benchmark::Fixture {
  public:
-  void SetUp(const ::benchmark::State& state) override {
+  void SetUp(const ::benchmark::State &state) override {
     num_groups = 5;
     num_rows = state.range(0);
     num_features = state.range(1);
@@ -36,9 +37,18 @@ class Fixture : public benchmark::Fixture {
     phis.reset(new thrust::device_vector<float>(
         X.NumRows() * (X.NumCols() + 1) * num_groups));
   }
-  void TearDown(const ::benchmark::State& state) {
+  void TearDown([[maybe_unused]] const ::benchmark::State &state) {
     phis.reset();
     test_data.reset();
+  }
+  // to keep compiler happy
+  void SetUp(::benchmark::State &st) override {
+    SetUp(const_cast<const ::benchmark::State &>(st));
+  }
+
+  // to keep compiler happy
+  void TearDown(::benchmark::State &st) override {
+    TearDown(const_cast<const ::benchmark::State &>(st));
   }
   std::vector<PathElement<XgboostSplitCondition>> model;
   std::unique_ptr<TestDataset> test_data;
@@ -49,7 +59,7 @@ class Fixture : public benchmark::Fixture {
   size_t num_features;
 };
 
-BENCHMARK_DEFINE_F(Fixture, GPUTreeShap)(benchmark::State& st) { // NOLINT
+BENCHMARK_DEFINE_F(Fixture, GPUTreeShap)(benchmark::State &st) { // NOLINT
   for (auto _ : st) {
     GPUTreeShap(X, model.begin(), model.end(), num_groups, phis->begin(),
                 phis->end());
@@ -62,7 +72,7 @@ BENCHMARK_REGISTER_F(Fixture, GPUTreeShap)
     ->Args({100000, 500, 20, 10000});
 
 BENCHMARK_DEFINE_F(Fixture, GPUTreeShapInterventional)
-(benchmark::State& st) {  // NOLINT
+(benchmark::State &st) { // NOLINT
   TestDataset R_test_data(1000, num_features, 1429);
   DenseDatasetWrapper R = R_test_data.GetDeviceWrapper();
   for (auto _ : st) {
@@ -75,7 +85,8 @@ BENCHMARK_REGISTER_F(Fixture, GPUTreeShapInterventional)
     ->Args({1000, 10, 6, 1000})
     ->Args({10000, 50, 10, 1000});
 
-BENCHMARK_DEFINE_F(Fixture, GPUTreeShapInteractions)(benchmark::State& st) {// NOLINT
+BENCHMARK_DEFINE_F(Fixture, GPUTreeShapInteractions)
+(benchmark::State &st) { // NOLINT
   phis.reset(new thrust::device_vector<float>(X.NumRows() * (X.NumCols() + 1) *
                                               (X.NumCols() + 1) * num_groups));
   for (auto _ : st) {
@@ -91,7 +102,7 @@ BENCHMARK_REGISTER_F(Fixture, GPUTreeShapInteractions)
     ->Args({1000, 250, 20, 10000});
 
 BENCHMARK_DEFINE_F(Fixture, GPUTreeShapTaylorInteractions)
-(benchmark::State& st) {// NOLINT
+(benchmark::State &st) { // NOLINT
   phis.reset(new thrust::device_vector<float>(X.NumRows() * (X.NumCols() + 1) *
                                               (X.NumCols() + 1) * num_groups));
   for (auto _ : st) {
@@ -110,13 +121,13 @@ std::vector<int> GenerateCounts(size_t n, size_t max_depth) {
   std::mt19937 gen(95);
   std::uniform_int_distribution<int> distrib(0, max_depth - 1);
   std::vector<int> out(n);
-  for (auto& x : out) {
+  for (auto &x : out) {
     x = distrib(gen);
   }
   return out;
 }
 
-static void BFDBinPacking(benchmark::State& state) {// NOLINT
+static void BFDBinPacking(benchmark::State &state) { // NOLINT
   size_t n = state.range(0);
   size_t max_depth = state.range(1);
   thrust::device_vector<int> counts = GenerateCounts(n, max_depth);
--- ci/gpu/build.sh
+++ ci/gpu/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright (c) 2020, NVIDIA CORPORATION.
+# Copyright (c) 2022, NVIDIA CORPORATION.
 #########################################
 # GPUTreeShap GPU build and test script for CI #
 #########################################
@@ -25,23 +25,6 @@ cd ~
 # SETUP - Check environment
 ################################################################################
 
-gpuci_logger "Install cmake"
-mkdir cmake
-cd cmake
-wget https://github.com/Kitware/CMake/releases/download/v3.18.2/cmake-3.18.2-Linux-x86_64.sh
-sh cmake-3.18.2-Linux-x86_64.sh --skip-license
-export PATH=$PATH:$PWD/bin
-cd ..
-
-gpuci_logger "Install gtest"
-wget https://github.com/google/googletest/archive/release-1.10.0.zip
-unzip release-1.10.0.zip
-mv googletest-release-1.10.0 gtest && cd gtest
-cmake . && make
-cp -r googletest/include include
-export GTEST_ROOT=$PWD
-cd ..
-
 gpuci_logger "Check environment"
 env
 
@@ -52,16 +35,16 @@ nvidia-smi
 $CC --version
 $CXX --version
 
+gpuci_logger "Activate conda env"
+. /opt/conda/etc/profile.d/conda.sh
+conda activate rapids
 
 ################################################################################
 # BUILD - Build tests
 ################################################################################
 
 gpuci_logger "Build C++ targets"
-mkdir $WORKSPACE/build
-cd $WORKSPACE/build
-cmake .. -DBUILD_GTEST=ON -DBUILD_EXAMPLES=ON -DBUILD_BENCHMARKS=ON
-make -j
+./build.sh
 
 ################################################################################
 # TEST - Run GoogleTest
