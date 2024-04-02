fc777572f5c1850ecfbfe05f4a268f8600f9f231
Bradley Dice <bdice@bradleydice.com>
2024-04-02 18:36:37 -0500

    Add dependency on CUDA. (#46)
    * Add dependency on CUDA.
    
    * Pin same compilers as rest of RAPIDS.
    
    * Remove extra else().
    
    * Use other compiler packages.
    
    * Remove sysroot.
    
    * Several changes to make CI pass.
    
    * Update to 24.04. Closes #47.
    
    * Fix lint.
    
    * Update style checks to use pre-commit.
    
    * Add checks to dependencies.yaml.
    
    * Cleanup.
    
    * Add checks file key.

--- ci/check_style.sh
+++ ci/check_style.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-# Copyright (c) 2023, NVIDIA CORPORATION.
+# Copyright (c) 2023-2024, NVIDIA CORPORATION.
 
-# Ignore errors
-set +e
-RETVAL="0"
+set -euo pipefail
 
+rapids-logger "Create checks conda environment"
 . /opt/conda/etc/profile.d/conda.sh
 
-# Check for a consistent code format
-pip install cpplint
-FORMAT=$(cpplint --recursive GPUTreeShap tests example benchmark 2>&1)
-FORMAT_RETVAL=$?
-if [ "$RETVAL" = "0" ]; then
-  RETVAL=$FORMAT_RETVAL
-fi
+ENV_YAML_DIR="$(mktemp -d)"
 
-# Output results if failure otherwise show pass
-if [ "$FORMAT_RETVAL" != "0" ]; then
-  echo -e "\n\n>>>> FAILED: cpplint format check; begin output\n\n"
-  echo -e "$FORMAT"
-  echo -e "\n\n>>>> FAILED: cpplint format check; end output\n\n"
-else
-  echo -e "\n\n>>>> PASSED: cpplint format check\n\n"
-fi
+rapids-dependency-file-generator \
+  --output conda \
+  --file_key checks \
+  --matrix "cuda=${RAPIDS_CUDA_VERSION%.*};arch=$(arch);py=${RAPIDS_PY_VERSION}" | tee "${ENV_YAML_DIR}/env.yaml"
 
-exit $RETVAL
+rapids-mamba-retry env create --force -f "${ENV_YAML_DIR}/env.yaml" -n checks
+conda activate checks
+
+# Run pre-commit checks
+pre-commit run --all-files --show-diff-on-failure
